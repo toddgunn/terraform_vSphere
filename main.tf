0f6515a02274a52e4bbf4f50c5f590f7a01cb571
toddgunn <todd.k.gunn@gmail.com>
2021-07-07 10:14:00 -0600

    commit

--- main.tf
+++ main.tf
@@ -1,1 +1,47 @@
-hello world
+# Configure the VMware vSphere Provider
+provider "vsphere" {
+  user           = "toddgunn"
+  password       = "p1p1n5THeG8tentert@iner"
+  vsphere_server = "157.201.228.240" 
+
+  # if you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+  name = "dc1"
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = "datastore1"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_resource_pool" "pool" {
+  name          = "cluster1/Resources"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = "public"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = "terraform-test"
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+
+  num_cpus = 2
+  memory   = 1024
+  guest_id = "other3xLinux64Guest"
+
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+
+  disk {
+    label = "disk0"
+    size  = 20
+  }
+}
